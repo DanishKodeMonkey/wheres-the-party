0db92d90c8942d59ac41f3da32faf64650e369ce
danishkodemonkey <humberjini@hotmail.com>
2024-06-25 13:29:58 +0200

    add mock mongoDB database objects for warrior and bard for testing

--- mockDatabase.js
+++ mockDatabase.js
@@ -11,4 +11,28 @@ export const characters = [
         ],
         isHit: false,
     },
+    {
+        name: 'warrior',
+        coordinates: [
+            { row: 37, col: 39 },
+            { row: 37, col: 40 },
+            { row: 38, col: 39 },
+            { row: 38, col: 40 },
+            { row: 39, col: 39 },
+            { row: 39, col: 40 },
+        ],
+        isHit: false,
+    },
+    {
+        name: 'bard',
+        coordinates: [
+            { row: 17, col: 9 },
+            { row: 17, col: 10 },
+            { row: 18, col: 9 },
+            { row: 18, col: 10 },
+            { row: 19, col: 9 },
+            { row: 19, col: 10 },
+        ],
+        isHit: false,
+    },
 ];
