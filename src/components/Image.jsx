53c9975c1041dc0db4bf646eb942c804ca4b0f02
danishkodemonkey <humberjini@hotmail.com>
2024-06-27 13:29:31 +0200

    feat: add image load state and signal to app

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,19 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 /* 
 Component responsible for fetching and preparing relevant image for rendering.
 
 */
 
-const Image = ({ src, alt }) => {
+const Image = ({ src, alt, onLoad }) => {
+    const handleImageLoad = () => {
+        onLoad();
+    };
     /* TODO handle API fetch of image and prep work here to serve to app */
     /* For now just serve local image */
+
     return (
         <div className='relative'>
             <img
                 src={src}
                 alt={alt}
                 className={`w-full h-auto object-cover`}
+                onLoad={handleImageLoad}
                 style={{
                     maxHeight: '1024px',
                     maxWidth: '1024px',
